2a006e4d361372ac4704172e13d084569149a503
kaiquesilvadev <j.kaique1570@gmail.com>
2024-04-10 20:41:35 -0300

    Feign client

---
+++ hr-payroll/src/main/java/com/kaique/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,22 @@
+package com.kaique.hrpayroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.kaique.hrpayroll.entities.dto.Worker;
+
+@Component
+
+//@FeignClient é uma anotação do Spring Cloud Feign que marca uma interface como um cliente Feign.
+//Isso significa que essa interface será usada para fazer chamadas a um serviço REST específico.
+//O parâmetro 'name' define um nome único para o cliente Feign, enquanto 'path' especifica o contexto do caminho base para todas as solicitações.
+//Neste exemplo, estamos definindo um cliente Feign chamado "hr-worker" que fará solicitações para o contexto "/workers" em sua URL base.
+@FeignClient(name = "hr-worker", url = "http://localhost:8080" ,  path = "/workers")
+public interface WorkerFeignClient {
+
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+}
