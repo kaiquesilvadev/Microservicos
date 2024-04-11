46344ae3897a4bc5a07a660cf787f7716fac198d
kaiquesilvadev <j.kaique1570@gmail.com>
2024-04-11 20:13:07 -0300

    Balanceamento de carga com Ribbon

--- hr-payroll/src/main/java/com/kaique/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/kaique/hrpayroll/feignclients/WorkerFeignClient.java
@@ -14,7 +14,7 @@ import com.kaique.hrpayroll.entities.dto.Worker;
 //Isso significa que essa interface será usada para fazer chamadas a um serviço REST específico.
 //O parâmetro 'name' define um nome único para o cliente Feign, enquanto 'path' especifica o contexto do caminho base para todas as solicitações.
 //Neste exemplo, estamos definindo um cliente Feign chamado "hr-worker" que fará solicitações para o contexto "/workers" em sua URL base.
-@FeignClient(name = "hr-worker", url = "http://localhost:8080" ,  path = "/workers")
+@FeignClient(name = "hr-worker" ,  path = "/workers")
 public interface WorkerFeignClient {
 
 	@GetMapping(value = "/{id}")
--- hr-worker/src/main/java/com/kaique/hrworker/controlles/WorkerController.java
+++ hr-worker/src/main/java/com/kaique/hrworker/controlles/WorkerController.java
@@ -2,7 +2,10 @@ package com.kaique.hrworker.controlles;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +18,14 @@ import com.kaique.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 
 	@Autowired
 	private WorkerRepository repository;
+	
+	@Autowired
+	private Environment env;
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
@@ -27,7 +35,8 @@ public class WorkerController {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-	
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}	
