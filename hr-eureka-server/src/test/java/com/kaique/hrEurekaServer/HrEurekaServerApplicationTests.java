91ed616028828bf7287c7e94055caec37d381015
kaiquesilvadev <j.kaique1570@gmail.com>
2024-04-12 19:30:01 -0300

    Criando projeto hr-eureka-server

---
+++ hr-eureka-server/src/test/java/com/kaique/hrEurekaServer/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.hrEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
